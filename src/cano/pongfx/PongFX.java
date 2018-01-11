3a59e4b36669da7fdb32be07ec1461aa431ee3fd
Juanma El Cano <Juanma El Cano@JuanmaElCano>
2018-01-11 10:14:08 +0100

    11/01/2018 CREAR Y MODIFICAR FONDO Y CIRCULO

--- src/cano/pongfx/PongFX.java
+++ src/cano/pongfx/PongFX.java
@@ -1,16 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/*PAQUETE DONDE SE ENCUENTRA EL ARCHIVO*/
 package cano.pongfx;
 
+/*LOS NOMBRES DE LAS CLASES SIEMPRE EMPIEZAN POR MAYUSCULA*/
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+/*CONTENEDOR PARA LA BOLA*/
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+/*CLASE PARA LA BOLA*/
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 /**
@@ -21,31 +19,20 @@ public class PongFX extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
+        /*LIENZO DONDE ESTA LA BOLA*/
+        Pane root = new Pane();
+        /*EL TAMAÑO DE LA VENTANA*/
+        Scene ventana = new Scene(root, 600, 400);
+        /*CAMBIO DE COLOR DE LA VENTANA*/
+        ventana.setFill(Color.BLACK);
+        primaryStage.setTitle("PongFX");
+        primaryStage.setScene(ventana);
         primaryStage.show();
+        /*CREAR CIRCULO*/
+        Circle ball = new Circle(10, 30, 7);
+        /*COLOR DEL CIRCULO*/
+        ball.setFill(Color.RED);
+        /*METER BOLA EN EL LAYOUT*/
+        root.getChildren().add(ball);
     }
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        launch(args);
-    }
-    
 }
